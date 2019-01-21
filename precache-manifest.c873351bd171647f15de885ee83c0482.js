534ac98fd0019061508e46d57b439b047390f891
lior cohen <liorco70@yahoo.com>
2019-01-21 11:29:04 +0100

    Updates

---
+++ precache-manifest.c873351bd171647f15de885ee83c0482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91a96d32044725ea72c65696723984cc",
+    "url": "/static/media/180825_01_laptop.91a96d32.mp4"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ecade311962ad5d29076",
+    "url": "/static/js/main.ecade311.chunk.js"
+  },
+  {
+    "revision": "274aac91ce3055fce730",
+    "url": "/static/js/1.274aac91.chunk.js"
+  },
+  {
+    "revision": "ecade311962ad5d29076",
+    "url": "/static/css/main.0cf766fc.chunk.css"
+  },
+  {
+    "revision": "24f29608405f40f705837fb2bf7226a8",
+    "url": "/index.html"
+  }
+];
